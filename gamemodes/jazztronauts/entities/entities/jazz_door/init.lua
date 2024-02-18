ac3c7a2857fc18fc1632c4b1e2318e607749da71
Sierra Foxtrot <sagejfox@aol.com>
2024-02-18 03:48:02 -0600

    Fixed jazz_door not having physics prop interactions*
    *At least, when it's set to alternative models. Turns out the default door's phys model is fucked so gonna have to fix that.

--- gamemodes/jazztronauts/entities/entities/jazz_door/init.lua
+++ gamemodes/jazztronauts/entities/entities/jazz_door/init.lua
@@ -19,8 +19,8 @@ local outputs =
 }
 
 function ENT:Initialize()
+	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_NONE)
-	self:SetSolid(SOLID_VPHYSICS)
 	self:SetUseType(SIMPLE_USE)
 	self:DrawShadow( false )
 
