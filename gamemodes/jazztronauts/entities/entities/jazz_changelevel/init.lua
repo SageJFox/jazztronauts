9224464c0a53a5311f57189fcfef0985b1e40752
Sierra Foxtrot <sagejfox@aol.com>
2024-05-27 10:13:12 -0500

    cleanup in jazz_changelevel
    wasn't ultimately the problem but fuck it, looks nicer

--- gamemodes/jazztronauts/entities/entities/jazz_changelevel/init.lua
+++ gamemodes/jazztronauts/entities/entities/jazz_changelevel/init.lua
@@ -3,28 +3,25 @@ ENT.Type = "point"
 ENT.DisableDuplicator = true
 
 function ENT:Initialize()
-
+	--self:AddEFlags(EFL_KEEP_ON_RECREATE_ENTITIES)
 end
 
 function ENT:KeyValue( key, value )
 	if key == "level" then self.Level = value end
 end
 
+local dest =
+{
+	["<hub>"] = function() return mapcontrol.GetHubMap() end,
+	["<encounter>"] = function() return mapcontrol.GetEncounterMap() end,
+	["<intro>"] = function() return mapcontrol.GetIntroMap() end,
+	["<outro1>"] = function() return mapcontrol.GetEndMaps()[1] end,
+	["<outro2>"] = function() return mapcontrol.GetEndMaps()[2] end,
+}
 
 function ENT:ChangeLevel( activator, caller, data )
-	if self.Level == "<hub>" then
-		mapcontrol.Launch(mapcontrol.GetHubMap())
-	elseif self.Level == "<encounter>" then
-		mapcontrol.Launch(mapcontrol.GetEncounterMap())
-	elseif self.Level == "<intro>" then
-		mapcontrol.Launch(mapcontrol.GetIntroMap())
-	elseif self.Level == "<outro1>" then
-		mapcontrol.Launch(mapcontrol.GetEndMaps()[1])
-	elseif self.Level == "<outro2>" then
-		mapcontrol.Launch(mapcontrol.GetEndMaps()[2])
-	else
-		mapcontrol.Launch(self.Level)
-	end
+	local level = dest[self.Level] and dest[self.Level]() or self.Level
+	mapcontrol.Launch(level)
 end
 
 function ENT:AcceptInput( name, activator, caller, data )
